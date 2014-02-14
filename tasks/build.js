e613fae6cfd090480ffb8b4832628156336c438c
clement <maboiteaspam@wanadoo.fr>
2014-02-15 02:06:14 +0800

    Add a new Facade : Phantomizer

--- tasks/build.js
+++ tasks/build.js
@@ -10,15 +10,7 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask("phantomizer-confess", "Measure page loading times", function () {
 
-    var webserver           = ph_libutil.webserver;
-    var router_factory      = ph_libutil.router;
-    var optimizer_factory   = ph_libutil.optimizer;
-    var meta_factory        = ph_libutil.meta;
-
-    var config = grunt.config.get();
-
     var options = this.options({
-      meta_dir:'',
       web_server_paths:[],
       in_request:'',
       host:'localhost',
@@ -31,17 +23,11 @@ module.exports = function(grunt) {
 
     var in_request = options.in_request;
     var web_server_paths = options.web_server_paths;
-    var meta_dir = options.meta_dir;
     var host = options.host;
     var port = options.port?options.port:"";
     var ssl_port = options.ssl_port?options.ssl_port:"";
     var action = options.action;
-    var web_server_log = options.web_server_log;
     var inject_assets = options.inject_assets;
-
-    var meta_manager = new meta_factory(process.cwd(), meta_dir);
-    var optimizer = new optimizer_factory(meta_manager, config, grunt);
-    var router = new router_factory(config.routing);
 
 
     var done = this.async();
@@ -52,19 +38,15 @@ module.exports = function(grunt) {
       } else {
         grunt.log.writeln(stdout);
       }
-      if(webserver.stop){
-        webserver.stop(function(){
-          done();
-        });
-      }
-      else done();
+      done();
     }
 
     var target_url = in_request;
     if( in_request.match(/^http/) == null ){
-      router.load(function(){
+// get phantomizer main instance
+      var phantomizer = ph_libutil.get("main");
 
-        webserver = new webserver(router,optimizer,meta_manager,grunt, web_server_paths);
+      phantomizer.create_webserver(web_server_paths,function(webserver){
         webserver.enable_dashboard(false);
         webserver.enable_build(false);
         webserver.enable_assets_inject(inject_assets);
@@ -74,9 +56,14 @@ module.exports = function(grunt) {
         in_request = in_request.substring(1)=="/"?in_request:"/"+in_request;
         target_url = "http://"+host+(port?":"+port:port)+in_request;
         grunt.log.ok("Running "+target_url);
-        run_confess(target_url, action,finish);
+        run_confess(target_url, action, function(stderr,stdout){
+          if(webserver.stop){
+            webserver.stop(function(stderr,stdout){
+              finish();
+            });
+          }
+        });
       });
-
     }else{
       run_confess(in_request,action,finish);
     }
